035dc5180ef402b035bdb22d2671115ac27e57fb
YusufSizmaz <yusuf.sizmaz11@gmail..com>
2025-02-04 16:07:22 +0300

    Hiçbir değişiklik yapılmadan dosyalar commit edildi.

---
+++ src/components/Currency.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import "./currency.css";
+import { FaArrowRightArrowLeft } from "react-icons/fa6";
+import axios from "axios";
+
+let BASE_URL = "https://api.freecurrencyapi.com/v1/latest";
+let API_KEY = "fca_live_5DxwsFzjxykIJIhAv5NfK43Gknw0Ut3Ovbqiavsn";
+
+function Currency() {
+  const [amount, setAmaount] = useState(0);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("TRY");
+  const [result, setResult] = useState(0);
+
+  const exchange = async () => {
+    const response = await axios.get(
+      `${BASE_URL}?apikey=${API_KEY}&base_currency=${fromCurrency}`
+    );
+
+    setResult(response.data.data[toCurrency] * amount);
+  };
+
+  return (
+    <div className="currency-div">
+      <div className="baslik">
+        <h3>Canlı Döviz Kuru</h3>
+      </div>
+
+      <div style={{ marginTop: "25px" }}>
+        <input
+          value={amount}
+          onChange={(e) => setAmaount(e.target.value)}
+          type="number"
+          min={0}
+          className="amount"
+        />
+
+        <select
+          onChange={(e) => setFromCurrency(e.target.value)}
+          className="from-currency-option"
+        >
+          <option>USD</option>
+          <option>EUR</option>
+          <option>TRY</option>
+        </select>
+        <FaArrowRightArrowLeft
+          style={{
+            fontSize: "20px",
+            color: "#fff",
+            marginRight: "10px",
+            marginTop: "10px",
+          }}
+        />
+
+        <select
+          onChange={(e) => setToCurrency(e.target.value)}
+          className="to-currency-option"
+        >
+          <option>TRY</option>
+          <option>EUR</option>
+          <option>USD</option>
+        </select>
+
+        <input
+          value={result.toFixed(2)}
+          onChange={(e) => setResult(e.target.value)}
+          type="number"
+          className="result"
+          min={0}
+        />
+      </div>
+      <div>
+        <button onClick={exchange} className="exchange-button">
+          Çevir
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Currency;
